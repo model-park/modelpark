e6985b249783e699308f06e14d332de913a458c7
Veysel Kocaman <vkocaman@gmail.com>
2024-04-28 11:49:40 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,68 @@
+
+# ModelPark
+
+ModelPark provides a versatile platform to share and manage your ML models directly from your machine, offering a convenient Python API to manage these tasks programmatically, including controlling access and publishing applications.
+
+## Features
+
+- Share models directly from the Python API.
+- Publish and manage applications using the ModelPark Python API.
+- Configure access management according to your needs through Python methods.
+
+## Installation
+
+To install ModelPark, you can use pip:
+```bash
+pip install modelpark
+```
+
+## Configuration
+
+Ensure Python and pip are installed on your machine. This API interfaces with the ModelPark CLI but manages interactions programmatically through Python.
+
+## Usage
+
+Here's how you can use the ModelPark Python package:
+
+### Initialize and Login
+```python
+from modelpark import ModelPark
+
+mp = ModelPark()
+mp.login(username="your_username", password="your_password")
+mp.init(port=8080, detach=True)
+```
+
+### Register an Application
+```python
+mp.register(port=3000, name="my-app", file_path="~/my-app/streamlit-app.py", access="public", framework="streamlit")
+```
+
+### List Registered Applications
+```python
+mp.ls()
+```
+
+### Make an API Call to a Registered Application
+```python
+user_credentials = {'username': 'your_username', 'password': 'your_password'}
+app_name = 'my-app'
+payload = {'key': 'value'}  # Payload required by the application
+
+# Make the API call
+response = mp.make_api_call(app_name, user_credentials, payload)
+print(response.json())  # Assuming the response is in JSON format
+```
+
+### Stop and Logout
+```python
+mp.stop()
+mp.logout()
+```
+
+### Kill an Application
+```python
+mp.kill(name="my-app")
+```
+
+This API provides a more Pythonic way of managing your applications with ModelPark compared to using the CLI directly.
---
+++ setup.py
@@ -0,0 +1,66 @@
+import os
+import platform
+import subprocess
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+class CustomInstall(install):
+    """Custom handler for the 'install' command."""
+
+    def run(self):
+        install.run(self)
+        self.install_system_specific_binary()
+
+    def install_system_specific_binary(self):
+        """Download and install the correct binary for the current OS."""
+        system = platform.system().lower()
+        if system == 'linux':
+            url = "https://modelpark.app/dist-folder/modelpark-cli-linux"
+            self.download_and_install(url)
+        elif system == 'darwin':  # macOS is identified as 'Darwin'
+            url = "https://modelpark.app/dist-folder/modelpark-cli-macos"
+            self.download_and_install(url)
+        else:
+            raise Exception(f"Unsupported operating system: {system}")
+
+    def download_and_install(self, url):
+        """Download and install binary from a given URL."""
+        target_dir = "/usr/local/bin/modelpark"
+        os.makedirs(os.path.dirname(target_dir), exist_ok=True)
+        subprocess.check_call(['curl', url, '-o', target_dir])
+        subprocess.check_call(['chmod', '+x', target_dir])
+        print(f"Installed binary to {target_dir}")
+
+
+
+setup(
+    name='modelpark',
+    version='0.1.0',
+    description='Versatile solution for sharing apps through secure URLs',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/model-park/modelpark',
+    author='Your Name',
+    author_email='info@modelpark.app',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='modelpark, deployment, cloud, api',
+    cmdclass={
+        'install': CustomInstall,
+    }
+)
+
